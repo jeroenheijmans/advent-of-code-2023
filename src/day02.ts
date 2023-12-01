456610543effe67f8033644685390120055d03bc
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-01 23:11:02 +0100

    Day 02 prep

---
+++ src/day02.ts
@@ -0,0 +1,19 @@
+import {start, finish} from './util.ts';
+start();
+
+const input = `NODATA`;
+
+const data = input
+  .trim()
+  .split(/\r?\n/)
+  ;
+
+const part1 = data
+  .map(x => x)
+
+const part2 = ""
+
+console.log("Part 1:", part1);
+console.log("Part 2:", part2);
+
+finish();
