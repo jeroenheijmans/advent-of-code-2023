454ebfcefdd203b9ce5342b7d33094f044d9c1ed
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-03 07:55:16 +0100

    Improve utils

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// import './day01.ts';
-// import './day02.ts';
+import './day01.ts';
+import './day02.ts';
 import './day03.ts';
  
--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,15 @@
 let timeout: number;
+let startedAt: Date;
 
 export function start(day: number, maxRunTime = 5000) {
-  console.log(`%cAdvent of Code DAY ${day.toString().padStart(2, "0")}, running at ${new Date().toISOString().replace("T", ", ")}`, 'color: lime');
+  startedAt = new Date();
+  console.log(`%cAdvent of Code DAY ${day.toString().padStart(2, "0")}, running at ${startedAt.toISOString().replace("T", ", ")}`, 'color: lime');
   timeout = setTimeout(() => { throw Error(`Script timed out after ${maxRunTime}ms!`); }, maxRunTime);
 }
 
 export function finish() {
+  const finishedIn = new Date().getTime() - startedAt.getTime();
+  console.log(`%cFinished in ${finishedIn}ms.`, 'color: gray');
   console.log("");
   clearTimeout(timeout);
 }
