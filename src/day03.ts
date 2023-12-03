5d18f7e86d70bf37e00ad7357a25fd6255b05685
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-03 07:46:24 +0100

    Day 03 cleanup

--- src/day03.ts
+++ src/day03.ts
@@ -1,5 +1,5 @@
 import {start, finish, add} from './util.ts';
-start(2);
+start(3);
 
 let input = `
 467..114..
@@ -21,7 +21,14 @@ const data = input
   .split(/\r?\n/)
   ;
 
-const objects: Record<string, any> = {};
+interface IEntry {
+  nr: string;
+  x: number;
+  y: number;
+  char?: string;
+}
+
+const objects: IEntry[] = [];
 
 let nr = "";
 data.forEach((line, y) => {
@@ -35,64 +42,39 @@ data.forEach((line, y) => {
       return;
     }
 
-    if (nr) {
-      const key = `${nrX};${y}`;
-      objects[key] = {nr, x: nrX, y, char: ""};
-    }
+    if (nr) objects.push({nr, x: nrX, y, char: ""});
 
     nr = "";
     nrX = x;
 
-    if (char === ".") {
-      return;
-    }
+    if (char === ".") return;
 
-    const key = `${x};${y}`;
-    objects[key] = {nr, x, y, char};
+    objects.push({nr, x, y, char});
   })
 
-  if (nr) {
-    const key = `${nrX};${y}`;
-    objects[key] = {nr, x: nrX, y};
-    nr = "";
-  }
-})
+  if (nr) objects.push({nr, x: nrX, y});
+});
 
-const relevant = new Set();
-
-for(const [key, val] of Object.entries(objects)) {
-  if (val.nr.match(/[0-9]/)) {
-    for (let dx = -1; dx < val.nr.length + 1; dx++) {
-      for (let dy = -1; dy < 2; dy++) {
-        const targetKey = `${val.x + dx};${val.y + dy}`;
-        if (objects[targetKey] && objects[targetKey].char) {
-          relevant.add(val);
-        }
-      }
+function areAdjacent(part1: IEntry, part2: IEntry) {
+  for (let dx = -1; dx < part1.nr.length + 1; dx++) {
+    for (let dy = -1; dy < 2; dy++) {
+      if (part1.x + dx === part2.x && part1.y + dy === part2.y) return true;
     }
   }
+  return false;
 }
 
-// console.log(objects)
+const part1 = objects
+  .filter(val => val.nr)
+  .filter(val => objects.filter(o => o.char).filter(o => areAdjacent(val, o)).length > 0)
+  .map(part => parseInt(part.nr))
+  .reduce(add, 0);
 
-const part1 = [...relevant].map(x => parseInt(x.nr)).reduce(add, 0);
-
-const part2 = Object.values(objects)
+const part2 = objects
   .filter(o => o.char === "*")
-  .map(o => ({
-    gear: o,
-    parts: Object.values(objects).filter(val => val.nr).filter(val => {
-      for (let dx = -1; dx < val.nr.length + 1; dx++) {
-        for (let dy = -1; dy < 2; dy++) {
-          if (val.x + dx === o.x && val.y + dy === o.y) return true;
-        }
-      }
-      return false;
-    })
-  }))
-  .filter(x => x.parts.length === 2)
-  // .map(x => x.gear)
-  .map(x => parseInt(x.parts[0].nr) * parseInt(x.parts[1].nr))
+  .map(gear => objects.filter(val => val.nr).filter(val => areAdjacent(val, gear)))
+  .filter(parts => parts.length === 2)
+  .map(parts => parseInt(parts[0].nr) * parseInt(parts[1].nr))
   .reduce(add, 0)
 ;
 
