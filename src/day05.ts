bf06a163e5534110a05d8779e11c449f12ff78c1
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-05 07:16:29 +0100

    Day 05 part 2

--- src/day05.ts
+++ src/day05.ts
@@ -2,7 +2,7 @@ import {start, finish} from './util.ts'
 start(5)
 
 let input = `
-seeds: 79 14 55 13
+seeds: 79 14
 
 seed-to-soil map:
 50 98 2
@@ -82,14 +82,116 @@ while (items.some(i => i.type !== "location")) {
   items.forEach((item, idx) => {
     let map = maps.find(m => m.from === item.type) as Map;
     let range = map.ranges.find(m => item.nr >= m.source && item.nr <= m.end);
-    // console.log("Moving", item)
     if (range) items[idx].nr = item.nr + range.diff;
     items[idx].type = map.to;
   })
 }
 
 const part1 = Math.min(...items.map(i => i.nr))
-const part2 = 0
+
+interface Slice {
+  type: string;
+  from: number;
+  to: number;
+  count: number;
+}
+
+let slices: Slice[] = [];
+
+for (let i = 0; i < seeds.length; i += 2) {
+  slices.push({
+    type: "seed",
+    from: seeds[i],
+    to: seeds[i] + seeds[i + 1] - 1,
+    count: seeds[i + 1],
+  })
+}
+
+while (slices.some(s => s.type !== "location")) {
+  let newSlices: Slice[] = [];
+
+  slices.forEach((slice, idx) => {
+    // console.log("Considering", slice)
+
+    let map = maps.find(m => m.from === slice.type) as Map;
+    let loRange = map.ranges.find(r => slice.from >= r.source && slice.from <= r.end);
+    let hiRange = map.ranges.find(r => slice.to >= r.source && slice.from <= r.end);
+
+    slice.type = map.to;
+
+    if (loRange && loRange === hiRange) {
+      slice.from = slice.from + loRange.diff;
+      slice.to = slice.to + loRange.diff;
+      newSlices.push(slice);
+    }
+
+    else if (loRange && !hiRange) {
+      newSlices.push({
+        from: slice.from + loRange.diff,
+        to: loRange.end,
+        type: map.to,
+        count: loRange.end - slice.from - loRange.diff,
+      })
+      newSlices.push({
+        from: loRange.end + 1,
+        to: slice.to,
+        type: map.to,
+        count: slice.to - loRange.end - 1,
+      })
+    }
+
+    else if (!loRange && hiRange) {
+      newSlices.push({
+        from: slice.from,
+        to: hiRange.target,
+        type: map.to,
+        count: hiRange.target - slice.from,
+      })
+      newSlices.push({
+        from: hiRange.target,
+        to: slice.to + hiRange.diff,
+        type: map.to,
+        count: slice.to + hiRange.diff - hiRange.target,
+      })
+    }
+
+    else if (loRange && hiRange) {
+      newSlices.push({
+        from: slice.from + loRange.diff,
+        to: loRange.end,
+        type: map.to,
+        count: loRange.end - slice.from - loRange.diff,
+      })
+      newSlices.push({
+        from: loRange.end + 1,
+        to: hiRange.target - 1,
+        type: map.to,
+        count: (hiRange.target - 1) - (loRange.end + 1),
+      })
+      newSlices.push({
+        from: hiRange.target,
+        to: slice.to + hiRange.diff,
+        type: map.to,
+        count: slice.to + hiRange.diff - hiRange.target,
+      })
+    }
+
+    else if (!loRange && !hiRange) {
+      newSlices.push(slice);
+    }
+
+    else {
+      throw "Unexpected situation";
+    }
+    
+  })
+
+  slices = newSlices
+}
+
+// console.log(slices);
+
+const part2 = Math.min(...slices.map(s => s.from))
 
 console.log("Part 1:", part1)
 console.log("Part 2:", part2)
