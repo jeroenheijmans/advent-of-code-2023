1318670d8e3cb104da24f2497c26c56fbacf9442
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-08 06:17:50 +0100

    Day 08 part 1

--- src/day08.ts
+++ src/day08.ts
@@ -2,14 +2,42 @@ import {start, finish} from './util.ts'
 start(8)
 
 let input = `
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
 `
+
+input = Deno.readTextFileSync("./src/inputs/day08.txt")
 
 const data = input
   .trim()
-  .split(/\r?\n/)
-  .filter(x => x)
+  .split(/\r?\n\r?\n/)
 
-const part1 = data
+const nav = data[0].split("")
+const map = data[1]
+  .split(/\r?\n/)
+  .map(x => x.replace(" =", "").replace(/[(),]/g, "").split(" "))
+  .reduce((result, next) => {
+    result[next[0]] = {"L": next[1], "R": next[2]};
+    return result
+  }, {} as any)
+
+let location = "AAA"
+let part1 = 0
+let i = 0
+let len = nav.length
+
+
+do {
+  const turn = nav[i % len]
+  // console.log("At", location, "turning", turn)
+  location = map[location][turn]
+  part1++
+  if (location === "ZZZ") break;
+} while (i++ < 1e9)
+
 const part2 = 0
 
 console.log("Part 1:", part1)
