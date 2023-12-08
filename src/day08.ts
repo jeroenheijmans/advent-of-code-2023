81bca7dafaa8bbdb337a3f7b33e92d8efd16332d
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-08 06:27:47 +0100

    Day 08 part 2 - Wrong (21251)

--- src/day08.ts
+++ src/day08.ts
@@ -20,25 +20,36 @@ const map = data[1]
   .split(/\r?\n/)
   .map(x => x.replace(" =", "").replace(/[(),]/g, "").split(" "))
   .reduce((result, next) => {
-    result[next[0]] = {"L": next[1], "R": next[2]};
+    result[next[0]] = {
+      "from": next[0],
+      "L": next[1],
+      "R": next[2],
+      isPart1EndNode: next[0] === "ZZZ",
+      isPart2EndNode: next[0].endsWith("Z"),
+      isPart2StartNode: next[0].endsWith("A"),
+    };
     return result
   }, {} as any)
 
-let location = "AAA"
-let part1 = 0
-let i = 0
-let len = nav.length
 
+function pathLen(location = "AAA", endNodeProp = "isPart1EndNode") {
+  let result = 0
+  let i = 0
+  let len = nav.length
+  do {
+    const turn = nav[i % len]
+    location = map[location][turn]
+    result++
+    if (map[location][endNodeProp]) return result
+  } while (i++ < 1e9)
+}
 
-do {
-  const turn = nav[i % len]
-  // console.log("At", location, "turning", turn)
-  location = map[location][turn]
-  part1++
-  if (location === "ZZZ") break;
-} while (i++ < 1e9)
+const part2Journeys = Object.values(map)
+  .filter(n => n.isPart2StartNode)
+  .map(n => pathLen(n.from, "isPart2EndNode"))
 
-const part2 = 0
+const part1 = pathLen();
+const part2 = Math.max(...part2Journeys)
 
 console.log("Part 1:", part1)
 console.log("Part 2:", part2)
