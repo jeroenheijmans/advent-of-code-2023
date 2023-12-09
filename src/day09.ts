e07c3f2bdf420a7e8870868e0b75421be0951863
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-09 21:43:50 +0100

    Day 09 cleanup

--- src/day09.ts
+++ src/day09.ts
@@ -9,14 +9,6 @@ let input = `
 
 input = Deno.readTextFileSync("./src/inputs/day09.txt")
 
-const lines = input
-  .trim()
-  .split(/\r?\n/)
-  .filter(x => x)
-  .map(line => line.split(" ").map(n => parseInt(n)))
-
-let part1 = 0
-
 function buildPyramid(line: number[]): number[][] {
   if (line.every(n => n === 0)) return [line]
   const nextLine = []
@@ -26,34 +18,34 @@ function buildPyramid(line: number[]): number[][] {
   return [line, ...buildPyramid(nextLine)]
 }
 
-// lines.forEach(line => {
-//   const pyramid = buildPyramid(line)
-//   for (let i = 0; i < pyramid.length; i++) {
-//     const index = pyramid.length - i - 1;
-//     if (i === 0) {
-//       pyramid[index].push(0)
-//     } else {
-//       const left = pyramid[index + 1].at(-1) || 0;
-//       const right = pyramid[index + 1].at(-2) || 0;
-//       const base = pyramid[index].at(-1) || 0;
-//       const extra = base + Math.abs(left - right)
-//       pyramid[index].push(extra)
-//       part1 += extra
-//     }
+const pyramids = input
+  .trim()
+  .split(/\r?\n/)
+  .filter(x => x)
+  .map(line => line.split(" ").map(n => parseInt(n)))
+  .map(line => buildPyramid(line))
 
-//     // console.log(" ".repeat(index * 3) + pyramid[index]
-//     //   .map(n => n.toString().padStart(6, " "))
-//     //   .join(" ")
-//     // )
-//   }
-//   // console.log("--------------------------------------------------")
-//   // console.log()
-// })
+let part1 = 0
+
+pyramids.forEach(pyramid => {
+  for (let i = 0; i < pyramid.length; i++) {
+    const index = pyramid.length - i - 1;
+    if (i === 0) {
+      pyramid[index].push(0)
+    } else {
+      const left = pyramid[index + 1].at(-1) || 0;
+      const right = pyramid[index + 1].at(-2) || 0;
+      const base = pyramid[index].at(-1) || 0;
+      const extra = base + Math.abs(left - right)
+      pyramid[index].push(extra)
+      part1 += extra
+    }
+  }
+})
 
 let part2 = 0
 
-lines.forEach(line => {
-  const pyramid = buildPyramid(line)
+pyramids.forEach(pyramid => {
   for (let i = 0; i < pyramid.length; i++) {
     const index = pyramid.length - i - 1;
     if (i === 0) {
@@ -64,16 +56,9 @@ lines.forEach(line => {
       const extra = base - above
       pyramid[index].unshift(extra)
     }
-
-    // console.log(" ".repeat(index * 3) + pyramid[index]
-    //   .map(n => n.toString().padStart(6, " "))
-    //   .join(" ")
-    // )
   }
 
   part2 += pyramid[0][0]
-  // console.log("--------------------------------------------------")
-  // console.log()
 })
 
 console.log("Part 1:", part1)
