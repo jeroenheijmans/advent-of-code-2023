69a94f1e6448dff3bd00bcf7a68c518a842ef870
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-14 06:13:32 +0100

    Day 14 part 1 (slow but correct!)

--- src/day14.ts
+++ src/day14.ts
@@ -1,17 +1,63 @@
-import {startDay, finishDay} from './util.ts'
+import {startDay, finishDay, add} from './util.ts'
 startDay(14)
 
 let input = `
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
 `
 
-// input = Deno.readTextFileSync("./src/inputs/day14.txt")
+input = Deno.readTextFileSync("./src/inputs/day14.txt")
 
 const data = input
   .trim()
   .split(/\r?\n/)
   .filter(x => x)
 
-const part1 = data
+interface Point {
+  key: string;
+  x: number;
+  y: number;
+}
+
+const rocks: Point[] = []
+const walls: Point[] = []
+
+for (let y = 0; y < data.length; y++) {
+  for (let x = 0; x < data[0].length; x++) {
+    const key = `${x};${y}`
+    if (data[y][x] === "O") rocks.push({key,x,y})
+    if (data[y][x] === "#") walls.push({key,x,y})
+  }
+}
+
+let i = 0;
+while (i++ < 1000) {
+  rocks.forEach(rock => {
+    const targetKey = `${rock.x};${rock.y - 1}`
+    if (rock.y > 0 && !walls.some(w => w.key === targetKey) && !rocks.some(w => w.key === targetKey)) {
+      rock.key = targetKey
+      rock.y = rock.y - 1
+    }
+  })
+}
+
+// for (let y = 0; y < data.length; y++) {
+//   let line = ""
+//   for (let x = 0; x < data[0].length; x++) {
+//     line += rocks.find(r => r.y === y && r.x === x) ? "O" : ".";
+//   }
+//   console.log(line)
+// }
+
+const part1 = rocks.map(r => data.length - r.y).reduce(add, 0)
 const part2 = 0
 
 console.log("Part 1:", part1)
