3aa462d2205266279d9f89204234d304b5a4a61e
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-14 06:18:48 +0100

    Day 14 part 1 refactors

--- src/day14.ts
+++ src/day14.ts
@@ -27,42 +27,49 @@ interface Point {
   y: number;
 }
 
-const rocks: Point[] = []
-const walls: Point[] = []
+function part1() {
+  const rocks: Point[] = []
+  const walls: Point[] = []
 
-for (let y = 0; y < data.length; y++) {
-  for (let x = 0; x < data[0].length; x++) {
-    const key = `${x};${y}`
-    if (data[y][x] === "O") rocks.push({key,x,y})
-    if (data[y][x] === "#") walls.push({key,x,y})
-  }
-}
-
-let hasMoved = true
-while (hasMoved) {
-  hasMoved = false
-  rocks.forEach(rock => {
-    const targetKey = `${rock.x};${rock.y - 1}`
-    if (rock.y > 0 && !walls.some(w => w.key === targetKey) && !rocks.some(w => w.key === targetKey)) {
-      hasMoved = true
-      rock.key = targetKey
-      rock.y = rock.y - 1
+  for (let y = 0; y < data.length; y++) {
+    for (let x = 0; x < data[0].length; x++) {
+      const key = `${x};${y}`
+      if (data[y][x] === "O") rocks.push({key,x,y})
+      if (data[y][x] === "#") walls.push({key,x,y})
     }
-  })
+  }
+
+  let hasMoved = true
+  while (hasMoved) {
+    hasMoved = false
+    rocks.forEach(rock => {
+      const targetKey = `${rock.x};${rock.y - 1}`
+      if (rock.y > 0 && !walls.some(w => w.key === targetKey) && !rocks.some(w => w.key === targetKey)) {
+        hasMoved = true
+        rock.key = targetKey
+        rock.y = rock.y - 1
+      }
+    })
+  }
+
+  // for (let y = 0; y < data.length; y++) {
+  //   let line = ""
+  //   for (let x = 0; x < data[0].length; x++) {
+  //     line += rocks.find(r => r.y === y && r.x === x) ? "O" : ".";
+  //   }
+  //   console.log(line)
+  // }
+
+  const part1 = rocks.map(r => data.length - r.y).reduce(add, 0)
+  console.log("Part 1:", part1)
 }
 
-// for (let y = 0; y < data.length; y++) {
-//   let line = ""
-//   for (let x = 0; x < data[0].length; x++) {
-//     line += rocks.find(r => r.y === y && r.x === x) ? "O" : ".";
-//   }
-//   console.log(line)
-// }
+function part2() {
+  const part2 = 0
+  console.log("Part 2:", part2)
+}
 
-const part1 = rocks.map(r => data.length - r.y).reduce(add, 0)
-const part2 = 0
-
-console.log("Part 1:", part1)
-console.log("Part 2:", part2)
+part1()
+part2()
 
 finishDay()
