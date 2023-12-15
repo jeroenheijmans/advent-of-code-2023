0bfec3fbb0e4ecdbbfc7b1a7c43ef230ddb5bc5e
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-15 06:12:22 +0100

    Day 15 part 1

--- src/day15.ts
+++ src/day15.ts
@@ -1,17 +1,32 @@
-import {startDay, finishDay} from './util.ts'
+import {startDay, finishDay, add} from './util.ts'
 startDay(15)
 
-let input = `
-`
-
-// input = Deno.readTextFileSync("./src/inputs/day15.txt")
+// const input = 'HASH'
+// const input = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
+const input = Deno.readTextFileSync("./src/inputs/day15.txt")
 
 const data = input
   .trim()
-  .split(/\r?\n/)
-  .filter(x => x)
+  .split(",")
 
 const part1 = data
+  .map(line => {
+    let result = 0
+    line.split("").forEach(c => {
+      const ascii = c.charCodeAt(0)
+      // if (ascii < 97 || ascii > 122) return
+      // console.log("Considering", c, ascii, 'result is now', result)
+      result += ascii
+      // console.log("Considering", c, ascii, 'result is now', result)
+      result *= 17
+      // console.log("Considering", c, ascii, 'result is now', result)
+      result %= 256
+    })
+    return result
+  })
+  .reduce(add, 0)
+
+
 const part2 = 0
 
 console.log("Part 1:", part1)
