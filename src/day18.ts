6601d7c1071fcc327307e8c43eea49c274b908b8
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-18 18:21:26 +0100

    Day 18 refactors

--- src/day18.ts
+++ src/day18.ts
@@ -1,4 +1,4 @@
-import {startDay, finishDay, drawGrid, Vector2, add} from './util.ts'
+import {startDay, finishDay, Vector2, add} from './util.ts'
 startDay(18)
 
 let input = `
@@ -18,7 +18,7 @@ L 2 (#015232)
 U 2 (#7a21e3)
 `
 
-input = Deno.readTextFileSync("./src/inputs/day18.txt")
+// input = Deno.readTextFileSync("./src/inputs/day18.txt")
 
 const raw = input
   .trim()
@@ -27,27 +27,21 @@ const raw = input
   .map(line => line.replaceAll(/[\(\)#]/g, ""))
   .map(line => line.split(" "))
 
-const dirs = {
-  "U": 3,
-  "L": 2,
-  "D": 1,
-  "R": 0,
-}
+const dirs = { "U": 3, "L": 2, "D": 1, "R": 0, }
 
-const data = raw
+const instructionsPart1 = raw
   .map(([direction, length, hex]) => ({
     direction: dirs[direction as "U"|"L"|"D"|"R"],
     distance: parseInt(length),
     hex
   }))
 
-const visited = new Set<string>() // outer scope so we can use it for part 2 debugging...
-
 function solve1() {
+  const visited = new Set<string>()
   let location = {x:0, y:0}
   const lookup = { "0;0": location } as Record<string, {x:number, y:number}>
 
-  data.forEach(instruction => {
+  instructionsPart1.forEach(instruction => {
     for (let i = 0; i < instruction.distance; i++) {
       location = {...location}
       if (instruction.direction === 3) location.y += -1
@@ -81,80 +75,93 @@ function solve1() {
     edges = newEdges
   }
 
-  const maxx = Math.max(...[...Object.values(lookup)].map(n => n.x)) + 1
-  const maxy = Math.max(...[...Object.values(lookup)].map(n => n.y)) + 1
-  const minx = Math.min(...[...Object.values(lookup)].map(n => n.x)) + 1
-  const miny = Math.min(...[...Object.values(lookup)].map(n => n.y)) + 1
-  for (let y = miny - 1; y < maxy; y++) {
-    let line = ""
-    for (let x = minx - 1; x < maxx; x++) {
-      line += lookup[`${x};${y}`] ? "#" : (visited.has(`${x};${y}`) ? "x" : ".")
+  function draw() {
+    const maxx = Math.max(...[...Object.values(lookup)].map(n => n.x)) + 1
+    const maxy = Math.max(...[...Object.values(lookup)].map(n => n.y)) + 1
+    const minx = Math.min(...[...Object.values(lookup)].map(n => n.x)) + 1
+    const miny = Math.min(...[...Object.values(lookup)].map(n => n.y)) + 1
+    for (let y = miny - 1; y < maxy; y++) {
+      let line = ""
+      for (let x = minx - 1; x < maxx; x++) {
+        line += lookup[`${x};${y}`] ? "#" : (visited.has(`${x};${y}`) ? "x" : ".")
+      }
+      console.log(line)
     }
-    console.log(line)
   }
 
+  draw()
   return visited.size + Object.values(lookup).length
 }
 
 const part1 = solve1()
 
-const instructions = raw
+const instructionsPart2 = raw
   .map(([_1, _2, hex]) => ({
     direction: parseInt(hex.split("")[hex.length - 1]),
     distance: parseInt(hex.substring(0, 5), 16)
   }))
 
-let from = {x:0, y:0}
-let to = {x:0, y:0}
+let target = {x:0, y:0}
 const borders = [] as {from:Vector2, to:Vector2}[]
-const corners = [to] as Vector2[]
 
-data // instructions
+instructionsPart1
   .forEach(instruction => {
-  from = {...to}
-  to = {...to}
-  if (instruction.direction === 0) to.x += +instruction.distance
-  if (instruction.direction === 1) to.y += +instruction.distance
-  if (instruction.direction === 2) to.x += -instruction.distance
-  if (instruction.direction === 3) to.y += -instruction.distance
-  borders.push({from, to})
-  corners.push(to)
-})
+    const current = {...target}
+    target = {...target}
+    if (instruction.direction === 0) target.x += +instruction.distance
+    if (instruction.direction === 1) target.y += +instruction.distance
+    if (instruction.direction === 2) target.x += -instruction.distance
+    if (instruction.direction === 3) target.y += -instruction.distance
+    
+    if (instruction.direction < 2) borders.push({ from: current, to: target })
+    else borders.push({ from: target, to: current })
+  })
 
-const minx = Math.min(...corners.map(c => c.x))
-const maxx = Math.max(...corners.map(c => c.x))
+const minx = Math.min(...borders.map(b => b.from.x))
+const maxx = Math.max(...borders.map(b => b.to.x))
+const miny = Math.min(...borders.map(b => b.from.y))
+const maxy = Math.max(...borders.map(b => b.to.y))
 
-const horizontalBorders = borders.filter(b => b.from.x !== b.to.x).toSorted((a,b) => a.from.y - b.from.y)
+const horizontalBorders = borders
+  .filter(b => b.from.x !== b.to.x)
+  .toSorted((a, b) => {
+    const dy = a.from.y - b.from.y
+    if (dy !== 0) return dy < 0 ? -1 : +1
+    const dx = a.from.x - b.from.x
+    if (dx !== 0) return dx < 0 ? -1 : +1
+    return 0
+  })
+
+const verticalBorders = borders
+  .filter(b => b.from.y !== b.to.y)
 
 let part2 = 0
 
 for (let x = minx; x <= maxx; x++) {
-  const relevantBorders = horizontalBorders.filter(b => Math.min(b.from.x, b.to.x) <= x && Math.max(b.to.x, b.from.x) >= x)
+  const relevantHorizontalBorders = horizontalBorders.filter(b => x >= b.from.x && x <= b.to.x)
+  const relevantVerticalBorders = verticalBorders.filter(b => b.from.x === x)
 
-  for (let idx = 0; idx < relevantBorders.length - 1; idx += 2) {
-    const border1 = relevantBorders[idx]
-    const border2 = relevantBorders[idx + 1]
-    const startx1 = Math.min(border1.from.x, border1.to.x)
-    const startx2 = Math.min(border2.from.x, border2.to.x)
-    const endx1 = Math.max(border1.from.x, border1.to.x)
-    const endx2 = Math.max(border2.from.x, border2.to.x)
+  let isInside = false
 
-    if (startx1 === startx2 && x === startx1) continue
-    if (endx1 === endx2 && x === endx1) continue
+  for (let y = miny - 1; y <= maxy + 1; y++) {
+    let justWentOutOfBounds = false
 
-    const nrOfFills = border2.from.y - border1.from.y - 1
-    // console.log("At", x, "adding", nrOfFills)
-    part2 += nrOfFills
+    if (relevantHorizontalBorders.length === 0) break
 
-    for (let y = border1.from.y; y <= border2.from.y; y++) {
-      visited.delete(`${x};${y}`)
+    if (relevantHorizontalBorders[0].from.y === y) {
+      justWentOutOfBounds = isInside
+      isInside = !isInside
+      const _ = relevantHorizontalBorders.shift()
     }
+
+    if (isInside || justWentOutOfBounds || relevantVerticalBorders.find(b => y >= b.from.y && y <= b.to.y)) {
+      // console.log("At", x, ",", y, "adding one")
+      part2++
+    }
+    // else console.log("  At", x, ",", y, "adding nothing")
   }
+  // console.log()
 }
-
-part2 += borders.map(b => Math.abs(b.from.x - b.to.x) + Math.abs(b.from.y - b.to.y)).reduce(add, 0)
-
-console.log([...visited].map(v => "[" + v.replace(";", ",") + "]").map(v => JSON.parse(v)))
 
 console.log("Part 1:", part1)
 console.log("Part 2:", part2)
