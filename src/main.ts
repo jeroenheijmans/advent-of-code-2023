cc39e3c9825cde4e26491cfce09046acce35348a
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-18 21:53:12 +0100

    Day 19 preparation

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@
 // import './day15.ts';
 // import './day16.ts';
 // import './day17.ts';
-import './day18.ts';
-// import './day19.ts';
+// import './day18.ts';
+import './day19.ts';
 // import './day20.ts';
 // import './day21.ts';
 // import './day22.ts';
