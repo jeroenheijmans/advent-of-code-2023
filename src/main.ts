83db45104ec64d960cce8ee7156c0af309622b3f
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-22 08:00:12 +0100

    Day 23 preparation

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@
 // import './day19.ts';
 // import './day20.ts';
 // import './day21.ts';
-import './day22.ts';
-// import './day23.ts';
+// import './day22.ts';
+import './day23.ts';
 // import './day24.ts';
 // import './day25.ts';
