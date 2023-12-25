83299a1b5dccb686084baf3323306741e9e8552c
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-25 07:09:50 +0100

    Day 25 part 1 (speed improvements, not enough)

--- src/day25.ts
+++ src/day25.ts
@@ -34,13 +34,31 @@ input
 
 function getGroups() {
   const wires = [...connections]
+
+  const nodes = wires.map(w => w.split(" ").flat())
+  const nodesToNrOfConnections = nodes.reduce((result, next) => {
+    result[next] = wires.filter(w => w.includes(next)).length
+    return result
+  }, {} as Record<string, number>)
+  const wiresToConnectedNess = wires.reduce((result, next) => {
+    const [part1, part2] = next.split(" ")
+    result[next] = nodesToNrOfConnections[part1] + nodesToNrOfConnections[part2]
+    return result
+  }, {} as Record<string, number>)
+
+  wires.sort((a, b) => wiresToConnectedNess[b] - wiresToConnectedNess[a])
+
   for (let i = 0; i < wires.length; i++) {
-    console.log("Working on group", i, "out of", wires.length)
+    console.log("Working on group", i, "out of", wires.length, `at ${new Date().toTimeString().substring(0, 12)}`)
     for (let j = i + 1; j < wires.length; j++) {
-      console.log("  Working on subgroup", j, "out of", wires.length, `at ${new Date().toTimeString()}`)
+      // console.log("  Working on subgroup", j, "out of", wires.length, `at ${new Date().toTimeString()}`)
       for (let k = j + 1; k < wires.length; k++) {
         // console.log("    Working on sub-subgroup", k, "out of", wires.length)
         const eliminated = [wires[i], wires[j], wires[k]]
+
+        const nodes = eliminated.map(x => x.split(" ")).flat()
+        if (nodes.some(node => nodesToNrOfConnections[node] > 5)) break;
+
         const candidate = wires.slice().filter(w => !eliminated.includes(w))
 
         const groups = [] as Set<string>[]
