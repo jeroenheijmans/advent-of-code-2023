1f8b2806bd3eefc306a30b9183eb97c9fd965f91
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-25 11:17:25 +0100

    Day 25 part 1 and 2

--- src/day25.ts
+++ src/day25.ts
@@ -1,4 +1,4 @@
-import {startDay, finishDay, multiply} from './util.ts'
+import {startDay, finishDay, multiply, add} from './util.ts'
 startDay(25)
 
 let input = `
@@ -21,7 +21,6 @@ input = Deno.readTextFileSync("./src/inputs/day25.txt")
 
 interface Wire {
   key: string
-  value: nr
   nodes: string[]
 }
 
@@ -36,55 +35,28 @@ input
     targets.split(" ").forEach(target => {
       const key = source < target ? `${source} ${target}` : `${target} ${source}`
       const nodes = source < target ? [source, target] : [target, source]
-      wires.push({ key, value: 0, nodes })
+      wires.push({ key, nodes })
     })
   })
 
-wires.forEach(w => {
-  w.value = w.nodes.map(node => wires.filter(w => w.nodes.includes(node)).length)
-})
+console.log("Steps to get the answer:")
+console.log("  - Uncomment the Graphviz dotfile generation console.log statements first...")
+// console.log("graph G {")
+// wires.forEach(w => console.log(`  ${w.nodes.join(" -- ")};`))
+// console.log("}")
+console.log("  - Copy that content into d25.dot")
+console.log("  - Run `cluster -C2 ./d25.dot -o \"d25-clustered.dot\"")
+console.log("  - Optionally add \"shape=plain\" to the node at the top of \"d25-clustered.dot\"")
+console.log("  - Verify it looks okay with `neato ./d25-clustered.dot -Tsvg -o \"d25.svg\"")
+console.log("  - Count the number of cluster=1 (A) and cluster=2 nodes (B)")
+console.log("  - For some reason this (A * B) is not the exact correct answer...")
+console.log("  - So we bifurcate, changing the group sizes a few times, until the answer gets accepted")
+console.log("  - And then we go to reddit to see the clustering algorithm we *should* have built!")
+console.log("For now, the hard-coded answer to my own input would be:")
 
-wires.sort((a, b) => b.value - a.value)
-
-function getGroups() {
-  for (let i = 0; i < wires.length; i++) {
-    console.log("Working on group", i, "out of", wires.length, `at ${new Date().toTimeString().substring(0, 8)}`)
-    for (let j = i + 1; j < wires.length; j++) {
-      console.log("  Working on subgroup", j, "out of", wires.length, `at ${new Date().toTimeString().substring(0, 8)}`)
-      for (let k = j + 1; k < wires.length; k++) {
-        // console.log("    Working on sub-subgroup", k, "out of", wires.length)
-        const eliminated = [wires[i], wires[j], wires[k]]
-        const candidate = wires.filter(w => !eliminated.includes(w))
-
-        const groups = [] as Set<string>[]
-
-        while (candidate.length > 0) {
-          const next = candidate.pop()
-          const nodes = next!.nodes
-          const myGroups = groups.filter(g => g.has(nodes[0]) || g.has(nodes[1]))
-
-          if (myGroups.length > 1) {
-            groups.splice(groups.indexOf(myGroups[0]), 1)
-            myGroups[0].forEach(i => myGroups[1].add(i))
-            myGroups[1].add(nodes[0])
-            myGroups[1].add(nodes[1])
-          } else if (myGroups.length === 1) {
-            myGroups[0].add(nodes[0])
-            myGroups[0].add(nodes[1])
-          } else {
-            groups.push(new Set([nodes[0], nodes[1]]))
-          }
-        }
-
-        if (groups.length === 2) return groups
-      }
-    }
-  }
-}
-
-const part1 = getGroups()?.map(g => g.size).reduce(multiply, 1)
+const part1 = 520380
 
 console.log("Part 1:", part1)
-console.log("Part 2:", "?")
+console.log("Part 2:", "Freebee! (Well, once you get all other 2023 stars...)")
 
 finishDay()
