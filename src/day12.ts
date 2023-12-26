b7e9c286e2853ea39f1eb1dc7eb522fb712216bc
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-26 22:33:47 +0100

    Day 12 part 2 (too slow, but with a next plan)

--- src/day12.ts
+++ src/day12.ts
@@ -24,44 +24,68 @@ const data = input
   }))
 
 function countOptions(pattern: string, nrs: number[]) {
-  function isStillPossible(current: string, pattern: string) {
-    for (let i = 0; i < current.length; i++) {
+
+  function isStillPossible(startCheckFrom: number, current: string) {
+    for (let i = startCheckFrom; i < current.length; i++) {
       if (pattern[i] !== "?" && pattern[i] !== current[i]) return false
     }
     return true
   }
 
-  function countVariations(length: number, nrs: number[], current: string) {
+  const maxIndex = nrs.length
 
-    if (nrs.length === 0) {
+  function countVariations(length: number, current: string, index: number) {
+    if (index === maxIndex) {
       const option = current.padEnd(length, ".")
-      if (!isStillPossible(option, pattern)) return 0
+      if (!isStillPossible(current.length, option)) return 0
       // console.log(option)
       return 1
     }
 
-    if (!isStillPossible(current, pattern)) return 0
+    const currentLength = current.length
+    const currentEndsWithMark = current[currentLength - 1] === "#"
+    const next = nrs[index]
 
-    const currentEndsWithMark = current.at(-1) === "#"
-    const [next, ...rest] = nrs
-    const minimumLengthForRest = rest.reduce(add, 0) - rest.length
-    const leftOverLength = length - current.length
+    let minimumLengthForRest = 0
+    for (let i = index + 1; i < nrs.length; i++) {
+      minimumLengthForRest += nrs[i] + 1
+    }
+
+    const leftOverLength = length - currentLength
     const maxLengthForNext = leftOverLength - minimumLengthForRest
     const maxOffset = maxLengthForNext - next
 
     let result = 0
     for (let offset = currentEndsWithMark ? 1 : 0; offset <= maxOffset; offset++) {
+      // Note to self to try to improve this...
+      // Let's say we're looking at a nr 3 for a max string length of 8
+      // A. Then all variants ending in "." can be grouped
+      // B. Then all variants ending in "#" (which is only 1, right?) can be grouped
+      // We can calculate only 1 option out of group A for follow-up options
+      // And multiply it to the number of variants for "rest" to get a tally
       const option = current + (".".repeat(offset) + "#".repeat(next))
-      result += countVariations(length, rest, option)
+      if (!isStillPossible(currentLength, option)) continue
+      result += countVariations(length, option, index + 1)
     }
     return result
   }
 
-  return countVariations(pattern.length, nrs, "")
+  return countVariations(pattern.length, "", 0)
 }
 
 const part1 = data.map(line => countOptions(line.pattern, line.nrs)).reduce(add, 0)
-const part2 = 0
+const part2 = data
+  .map(x => ({
+    pattern: [x.pattern, x.pattern, x.pattern, x.pattern, x.pattern].join("?"),
+    nrs: [...x.nrs, ...x.nrs, ...x.nrs, ...x.nrs, ...x.nrs] }))
+  .map((x, idx) => {
+    const start = new Date().getTime()
+    const result = countOptions(x.pattern, x.nrs)
+    const end = new Date().getTime()
+    console.log(`Line ${idx + 1} took ${((end-start)/1000).toFixed(3)} seconds`);
+    return result
+  })
+  .reduce(add, 0)
 
 console.log("Part 1:", part1)
 console.log("Part 2:", part2)
