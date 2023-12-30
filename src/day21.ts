009c4e53db60282696889873c775e21cdb0c6b8f
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-30 12:43:06 +0100

    Day 21 refactor to 0,0 based solution

--- src/day21.ts
+++ src/day21.ts
@@ -42,29 +42,33 @@ const data = input
   .filter(x => x)
   .map(line => line.split(""))
 
+const gridSize = data.length
+const midPoint = Math.floor(gridSize / 2)
+
 const grid = data
-  .map((line, y) => line.map((char, x) => ({
-    key: `${x};${y}`,
-    x,
-    y,
-    char,
-  })))
+  .map((line, y) => line
+    .map((char, x) => ({
+      x: x - midPoint,
+      y: y - midPoint,
+      char,
+    }))
+    .map(loc => ({ ...loc, key: `${loc.x};${loc.y}` }))
+  )
 
 const locations = grid
   .flat()
-
-const maxx = data[0].length, maxy = data.length
 
 const lookup = locations.reduce((result, next) => {
   result[next.key] = next
   return result
 }, {} as Record<string, Location>)
 
+
 const location0 = locations.find(l => l.char === "S") as Location
 const start = { x: location0.x, y: location0.y }
 location0.char = "."
 
-const maxi = 750
+const maxi = 64
 const directions = [{dx:-1, dy:0},{dx:+1, dy:0},{dx:0, dy:-1},{dx:0, dy:+1}]
 let options = [start]
 let part1 = 0
@@ -83,11 +87,11 @@ for (let i = 0; i < maxi; i++) {
       if (considered.has(newKey)) return
       considered.add(newKey)
 
-      const lookupX = newPos.x >= 0 ? (newPos.x % maxx) : (maxx + (newPos.x % maxx)) % maxx
-      const lookupY = newPos.y >= 0 ? (newPos.y % maxy) : (maxy + (newPos.y % maxy)) % maxy
+      const lookupX = newPos.x > midPoint ? (newPos.x > midPoint ? newPos.x - gridSize : newPos.x) : (newPos.x < -midPoint ? gridSize + newPos.x : newPos.x)
+      const lookupY = newPos.y > midPoint ? (newPos.y > midPoint ? newPos.y - gridSize : newPos.y) : (newPos.y < -midPoint ? gridSize + newPos.y : newPos.y)
       const target = lookup[`${lookupX};${lookupY}`]
 
-      if (!target) console.log(newPos, lookupX, lookupY)
+      if (!target) console.error("Found no target at newPos = ", newPos, " --- lookup:", lookupX, lookupY)
       if (target.char !== ".") return
 
       newOptions.push(newPos)
@@ -99,33 +103,17 @@ for (let i = 0; i < maxi; i++) {
   if (i === 63) part1 = options.length
 }
 
+console.log(locations.filter(l => l.char === "."&& (l.x + l.y) % 2 === 1 && 
+  (Math.abs(65 - l.x) + Math.abs(65 - l.y) <= 65)
+).length)
+
 
 function draw() {
-  const x1 = Math.min(...options.map(o => o.x))
-  const y1 = Math.min(...options.map(o => o.y))
-  const x2 = Math.max(...options.map(o => o.x))
-  const y2 = Math.max(...options.map(o => o.y))
-
-  const width = data[0].length
-  const height = data.length
-
-  const bonusx1 = Math.abs(Math.floor(x1 / width) - 1)
-  const bonusy1 = Math.abs(Math.floor(y1 / height) - 1)
-  const bonusx2 = Math.abs(Math.ceil(x2 / width))
-  const bonusy2 = Math.abs(Math.ceil(y2 / height))
-
-  const bonus = Math.max(bonusx1, bonusx2, bonusy1, bonusy2)
-
-  const fromx = -(bonus - 1) * width
-  const fromy = -(bonus - 1) * height
-  const tox = bonus * width
-  const toy = bonus * height
-
-  for (let y = fromy; y < toy; y++) {
+  for (let y = -maxi; y <= maxi; y++) {
     let line = ""
-    for (let x = fromx; x < tox; x++) {
-      const lookupX = x >= 0 ? (x % width) : (width + (x % width) - 1)
-      const lookupY = y >= 0 ? (y % height) : (height + (y % height) - 1)
+    for (let x = -maxi; x <= maxi; x++) {
+      const lookupX = x > midPoint ? (x > midPoint ? x - gridSize : x) : (x < -midPoint ? gridSize + x : x)
+      const lookupY = y > midPoint ? (y > midPoint ? y - gridSize : y) : (y < -midPoint ? gridSize + y : y)
       const target = lookup[`${lookupX};${lookupY}`]
       line += options.find(o => o.x === x && o.y === y) ? "O" : target.char.replace("#", " ").replace(".", "·")
       // const cls = options.find(o => o.x === x && o.y === y) ? "o" : target.char.replace("#", "x").replace(".", "p")
@@ -136,7 +124,7 @@ function draw() {
   }
 }
 
-// draw()
+draw()
 
 console.log("Part 1:", part1)
 console.log("Part 2:", 0, "(38893493797086 is 'too low')")
